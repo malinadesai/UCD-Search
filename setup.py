f327a889ef176986aa9c22d9ba1aaddd08455365
malinadesai <108042357+malinadesai@users.noreply.github.com>
2023-09-13 19:50:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,18 @@
+from setuptools import setup, find_packages
 
-#imports
+# check that python version is 3.8 or above
+python_version = sys.version_info
+if python_version < (3, 8):
+    sys.exit("Python < 3.8 is not supported, please install version 3.8.0 or higher.")
+print("Confirmed Python version {}.{}.{} >= 3.8.0".format(*python_version[:3]))
 
-# std lib
-from setuptools import setup
-import subprocess
-import sys
-import os
-from getpass import getpass
-import numpy as np
-import pylab as plt
-import matplotlib
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-import matplotlib.lines as mlines
-import matplotlib.style
-import matplotlib as mpl
-import pandas as pd
-from numpy.polynomial import polynomial as P
+setup(
+    name='UCD-Search',
+    version='1.0',
+    description='Set of ML models developed to find UCDs in Deep Sky Surveys'
+    author='Malina Desai'
+    author_email='mmdesai@mit.edu'
+    packages=find_packages()
+)
 
-# Data Lab
-from dl import authClient as ac, queryClient as qc, storeClient as sc
-
-# machine learning
-import sklearn
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, classification_report, \
-PrecisionRecallDisplay, RocCurveDisplay, f1_score, recall_score, plot_confusion_matrix
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.model_selection import train_test_split, KFold,RepeatedKFold, GridSearchCV, RandomizedSearchCV
-from sklearn.calibration import calibration_curve
-
-# others
-from dl.helpers.utils import convert
-import warnings
-from pathlib import Path
-from typing import Iterable, List, Optional, Union
 
